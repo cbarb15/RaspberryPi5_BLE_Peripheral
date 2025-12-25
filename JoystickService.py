d606b074042804387f880a4cef1f405379129b07
Charlie Barber <cbarb2747@gmail.com>
2025-12-24 16:02:51 -0800

    workng great

--- JoystickService.py
+++ JoystickService.py
@@ -1,12 +1,14 @@
 import bluetooth_gatt
 import bluetooth_constants
-from JoystickCharacteristic import  JoystickCharacteristic
-
+from LeftJoystickCharacteristic import  LeftJoystickCharacteristic
+from RightJoystickCharacteristic import RightJoystickCharacteristic
 
 class JoystickService(bluetooth_gatt.Service):
     def __init__(self, bus, path_base, index):
         print("Initialising JoystickService object")
         bluetooth_gatt.Service.__init__(self, bus, path_base, index,
         bluetooth_constants.JOYSTICK_SVC_UUID, True)
-        print("Adding JoystickCharacteristic to the service")
-        self.add_characteristic(JoystickCharacteristic(bus, 0, self))
+        print("Adding LeftJoystickCharacteristic to the service")
+        self.add_characteristic(LeftJoystickCharacteristic(bus, 0, self))
+        print("Adding RightJoystickCharacteristic to the service")
+        self.add_characteristic(RightJoystickCharacteristic(bus, 1, self))
