7fea1bb42242fc326ce9888b9e838a7057365d47
Charlie Barber <cbarb2747@gmail.com>
2026-03-24 19:53:43 -0700

    added thread to watch pullup line

--- main.py
+++ main.py
@@ -14,12 +14,19 @@ import serial
 import digitalio
 import time
 import multiprocessing
+import gpiod
+from gpiod.line import Direction, Value, Bias, Edge
+from gpiod.line_settings import LineSettings
+import select
+from datetime import timedelta
+import os
 
 bus = None
 adapter_path = None
 adv_mgr_interface = None
 uart_process = None
 mainloop = None
+chip_path = "/dev/gpiochip0"
 
 def register_ad_cb():
     print('Advertisement registered OK')
@@ -93,29 +100,87 @@ def start_advertising_and_create_GATT_app():
 def uart_callback():
    global uart
    while 1:
-      bytes_read = uart.read(8)
-      # if len(bytes_read) > 0:
-      print(f'byest read = {bytes_read}') 
+      # if uart.is_open and uart.in_waiting:
+      bytes_read = uart.read_all()
+      print(f'byest read = {bytes_read}')
          # data = "Advertise\n".encode('utf-8')
          # uart.write(data)
+
+def async_watch_line_value(chip_path, line_offset, done_fd):
+    # Assume a button connecting the pin to ground,
+    # so pull it up and provide some debounce.
+    with gpiod.request_lines(
+        chip_path,
+        consumer="async-watch-line-value",
+        config={
+            line_offset: gpiod.LineSettings(
+                edge_detection=Edge.BOTH,
+                bias=Bias.PULL_UP,
+                debounce_period=timedelta(milliseconds=10),
+            )
+        },
+    ) as request:
+        poll = select.poll()
+        poll.register(request.fd, select.POLLIN)
+        # Other fds could be registered with the poll and be handled
+        # separately using the return value (fd, event) from poll():
+        poll.register(done_fd, select.POLLIN)
+        while True:
+            for fd, _event in poll.poll():
+                if fd == done_fd:
+                    # perform any cleanup before exiting...
+                    return
+                # handle any edge events
+                for event in request.read_edge_events():
+                    print(
+                        "offset: {}  type: {:<7}  event #{}".format(
+                            event.line_offset, edge_type_str(event), event.line_seqno
+                        )
+                    )
+
+def uart_intterupt_task():
+   print("Starting interupt task")
+   done_fd = os.eventfd(0)
+   try:
+      async_watch_line_value("/dev/gpiochip0", 23, done_fd)
+   except OSError as ex:
+      print(ex, "\nCustomise the example configuration to suit your situation")
+      print("background thread exiting...")
+
 
 if __name__ == '__main__':   
    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
    bus = dbus.SystemBus()
+
+   # chip = gpiod.Chip(chip_path)   
+   # intterrupt_line_info = chip.get_line_info(23)
+   # print(intterrupt_line_info)
+   # interrupt_line = gpiod.request_lines(chip_path, consumer=None, config=
+   #    LineSettings(direction=Direction.OUTPUT)
+   # )
+   # print(f"Line {interrupt_line}")
+   # line_offset = 23
+   # with gpiod.request_lines(
+   #      chip_path,
+   #      consumer="get-line-value",
+   #      config={line_offset: LineSettings(direction=Direction.INPUT, output_value=Value.INACTIVE, active_low=True)},
+   #  ) as request:
+   #      print(chip.get_line_info(23))
+
+   uart_task = multiprocessing.Process(target=uart_intterupt_task)
+   uart_task.start()
 
    global uart
    try: 
       with serial.Serial("/dev/ttyAMA0", baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize= serial.EIGHTBITS, timeout=1) as uart:
          uart_process = multiprocessing.Process(target=uart_callback)
          time.sleep(1.0)
-         uart_process.start()
+         # uart_process.start()
    except serial.SerialException as e:
       serial.close()
       print(f"Error opening serial port: {e}")
    finally:
       uart.close()
-   
-   
 
    # we're assuming the adapter supports advertising
    adapter_path = bluetooth_constants.BLUEZ_NAMESPACE + bluetooth_constants.ADAPTER_NAME
