2edb6f5fac35a09ae32085f42fe83bcfb58561a3
Charlie Barber <cbarb2747@gmail.com>
2026-05-09 18:19:03 -0700

    working online for rising edge

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include <pybind11/embed.h>
+#include <wiringPi.h>
 
 namespace py = pybind11;
 using namespace std;
 
+void button_interrupt();
+
 int main() {
     py::scoped_interpreter guard{};
+    // wiringPiSetupGpio();
+    wiringPiSetup();
+    // pinMode(23, INPUT);
+    // pullUpDnControl(23, PUD_DOWN);
+    wiringPiISR(13, INT_EDGE_RISING, &button_interrupt);
 
-    py::module_ peripheral = py::module_::import("peripheral");
-    peripheral.attr("start_advertising_and_create_GATT_app")();
+    // py::module_ peripheral = py::module_::import("peripheral");
+    //peripheral.attr("start_advertising_and_create_GATT_app")();
+
+    while (1) {}
 
     return 0;
+}
+
+void button_interrupt() {
+    cout << "Button pushed" << endl;
 }
