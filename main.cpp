513afc21b14cc5881a98748f6cb2206784a47f90
Charlie Barber <cbarb2747@gmail.com>
2026-05-20 16:50:24 -0700

    sending uart datagit status

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <pybind11/embed.h>
 #include <wiringPi.h>
+#include <wiringSerial.h>
 #include <thread>
 #include <chrono>
+
+#include "WiringPi/wiringPi/wiringSerial.h"
 
 // #include "WiringPi/wiringPi/wiringPi.h"
 
@@ -14,14 +17,24 @@ using namespace std;
 void button_interrupt();
 
 int main() {
+    int fd;
+
     wiringPiSetupGpio();
 
     pinMode(IRQpin, INPUT);
     pullUpDnControl(IRQpin, PUD_UP);
 
     wiringPiISR(IRQpin, INT_EDGE_FALLING, &button_interrupt);
+    // fd = serialOpen("/dev/ttyAMA0", 115200);
+    // if (fd < 0) {
+    //     cout << "Unable to open serial device" << endl;
+    // }
 
-    while (1) {}
+    while (1) {
+       //  serialPutchar(fd, 'h');
+       //
+       // this_thread::sleep_for(std::chrono::seconds(3));
+    }
 }
 
 void button_interrupt() {
