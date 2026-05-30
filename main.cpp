d2755681e12cab76c036e817da01512014b7569c
Charlie Barber <cbarb2747@gmail.com>
2026-05-30 14:52:10 -0700

    all three buttons working perfectly

--- main.cpp
+++ main.cpp
@@ -13,47 +13,51 @@
 namespace py = pybind11;
 using namespace std;
 
-#define BLEButtonIrqPin     16
-#define AutoModeIrqPin      26
+#define BLE_BUTTON_IRQ_PIN  16
+#define AUTO_MODE_IRQ_PIN   26
+#define POWER_OFF_IRQ_PIN   19
 
-void BLEButtonInterrupt();
-void AutoModeEngageButtonInterrupt();
+void bleButtonInterrupt();
+void autoModeEngageButtonInterrupt();
+void powerOffButtonInterrupt();
 
 int main() {
     int fd;
 
     wiringPiSetupGpio();
 
-    pinMode(BLEButtonIrqPin, INPUT);
-    pullUpDnControl(BLEButtonIrqPin, PUD_DOWN);
-    pinMode(AutoModeIrqPin, INPUT);
-    pullUpDnControl(AutoModeIrqPin, PUD_DOWN);
+    pinMode(BLE_BUTTON_IRQ_PIN, INPUT);
+    pullUpDnControl(BLE_BUTTON_IRQ_PIN, PUD_DOWN);
+    pinMode(AUTO_MODE_IRQ_PIN, INPUT);
+    pullUpDnControl(AUTO_MODE_IRQ_PIN, PUD_DOWN);
+    pinMode(POWER_OFF_IRQ_PIN, INPUT);
+    pullUpDnControl(POWER_OFF_IRQ_PIN, PUD_DOWN);
 
-    wiringPiISR(BLEButtonIrqPin, INT_EDGE_RISING, &BLEButtonInterrupt);
-    wiringPiISR(AutoModeIrqPin, INT_EDGE_FALLING, &AutoModeEngageButtonInterrupt);
-  
+    wiringPiISR(BLE_BUTTON_IRQ_PIN, INT_EDGE_RISING, &bleButtonInterrupt);
+    wiringPiISR(AUTO_MODE_IRQ_PIN, INT_EDGE_RISING, &autoModeEngageButtonInterrupt);
+    wiringPiISR(POWER_OFF_IRQ_PIN, INT_EDGE_RISING, &powerOffButtonInterrupt);
+
     // fd = serialOpen("/dev/ttyAMA0", 115200);
     // if (fd < 0) {
     //     cout << "Unable to open serial device" << endl;
     // }
 
     while (1) {
-        this_thread::sleep_for(std::chrono::seconds(10));
-        // digitalWrite(AutoModeIrqPin, HIGH);
-        //
-        // this_thread::sleep_for(std::chrono::seconds(1));
-        // digitalWrite(AutoModeIrqPin, LOW);
-        // this_thread::sleep_for(std::chrono::seconds(1));
-    }
+
+   }
 }
 
-void BLEButtonInterrupt() {
+void bleButtonInterrupt() {
     // py::scoped_interpreter guard{};
     // py::module_ peripheralModule = py::module_::import("peripheral");
     cout << "Start Adv" << endl;
     // peripheralModule.attr("start_advertising_and_create_GATT_app")();
 }
 
-void AutoModeEngageButtonInterrupt() {
+void autoModeEngageButtonInterrupt() {
     cout << "Engage Auto Mode" << endl;
+}
+
+void powerOffButtonInterrupt() {
+    cout << "POWER OFF" << endl;
 }
